1c162dc070e2edd96b89956a634eb847bc5dc7d8
Doug Talbert <malieb42@students.tntech.edu>
2018-09-24 10:18:46 -0500

    Linked with LWJGL. Window class added for display

---
+++ src/main/Main.java
@@ -0,0 +1,48 @@
+package main;
+
+import window.Window;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Window window = new Window();
+		
+		window.run();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
